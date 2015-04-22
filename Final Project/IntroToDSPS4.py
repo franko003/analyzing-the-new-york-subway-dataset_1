5ea92b8818f5c556675330e7fb03a0b7e4cb313e
franko003 <franklettiere@gmail.com>
2015-04-21 20:02:18 -0500

    Committed by Udacity

---
+++ Final Project/IntroToDSPS4.py
@@ -0,0 +1,87 @@
+from pandas import *
+from ggplot import *
+
+def plot_weather_data(turnstile_weather):
+    '''
+    You are passed in a dataframe called turnstile_weather. 
+    Use turnstile_weather along with ggplot to make a data visualization
+    focused on the MTA and weather data we used in assignment #3.  
+    You should feel free to implement something that we discussed in class 
+    (e.g., scatterplots, line plots, or histograms) or attempt to implement
+    something more advanced if you'd like.  
+
+    Here are some suggestions for things to investigate and illustrate:
+     * Ridership by time of day or day of week
+     * How ridership varies based on Subway station
+     * Which stations have more exits or entries at different times of day
+       (You can use UNIT as a proxy for subway station.)
+
+    If you'd like to learn more about ggplot and its capabilities, take
+    a look at the documentation at:
+    https://pypi.python.org/pypi/ggplot/
+     
+    You can check out:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
+     
+    To see all the columns and data points included in the turnstile_weather 
+    dataframe. 
+     
+    However, due to the limitation of our Amazon EC2 server, we are giving you a random
+    subset, about 1/3 of the actual data in the turnstile_weather dataframe.
+    '''
+    
+    plot = ggplot(turnstile_weather, aes('Hour', 'ENTRIESn_hourly')) + geom_point() + ggtitle('Hourly Rides throughout the Day') + xlab('Time of day (military time)') + ylab('Total Hourly Entries')
+    
+    return plot
+
+from pandas import *
+from ggplot import *
+import numpy as np
+
+def plot_weather_data(turnstile_weather):
+    ''' 
+    plot_weather_data is passed a dataframe called turnstile_weather. 
+    Use turnstile_weather along with ggplot to make another data visualization
+    focused on the MTA and weather data we used in Project 3.
+    
+    Make a type of visualization different than what you did in the previous exercise.
+    Try to use the data in a different way (e.g., if you made a lineplot concerning 
+    ridership and time of day in exercise #1, maybe look at weather and try to make a 
+    histogram in this exercise). Or try to use multiple encodings in your graph if 
+    you didn't in the previous exercise.
+    
+    You should feel free to implement something that we discussed in class 
+    (e.g., scatterplots, line plots, or histograms) or attempt to implement
+    something more advanced if you'd like.
+
+    Here are some suggestions for things to investigate and illustrate:
+     * Ridership by time-of-day or day-of-week
+     * How ridership varies by subway station
+     * Which stations have more exits or entries at different times of day
+       (You can use UNIT as a proxy for subway station.)
+
+    If you'd like to learn more about ggplot and its capabilities, take
+    a look at the documentation at:
+    https://pypi.python.org/pypi/ggplot/
+     
+    You can check out the link 
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
+    to see all the columns and data points included in the turnstile_weather 
+    dataframe.
+     
+   However, due to the limitation of our Amazon EC2 server, we are giving you a random
+    subset, about 1/3 of the actual data in the turnstile_weather dataframe.
+    '''
+    entries_per_hr_df = turnstile_weather[['Hour', 'ENTRIESn_hourly']]
+    
+    mean_entries_per_hr_data = {'Hour': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                                 'Mean_Entries': []}
+    for i in range(0, 24):
+        df = entries_per_hr_df[entries_per_hr_df['Hour'] == i]
+        mean_entries = np.mean(df['ENTRIESn_hourly'])
+        mean_entries_per_hr_data['Mean_Entries'].append(mean_entries)
+    
+    mean_entries_per_hr_df = DataFrame(mean_entries_per_hr_data)
+    plot = ggplot(mean_entries_per_hr_df, aes('Hour', 'Mean_Entries')) + geom_point() + geom_line() + ggtitle('Average Daily Entries per Hour of the Day for May 2011') + xlab('Hour of Day (military)') + ylab('Average Daily Entries') + xlim(0, 23) + ylim(0, 3000)
+    return plot
+
